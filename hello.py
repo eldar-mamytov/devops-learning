df7822a3e0c1ca4c99cd83529d219946eeeba3a6
Eddie M <eldarmmtv@gmail.com>
2025-04-02 21:24:06 -0500

    Add add_numbers function

--- hello.py
+++ hello.py
@@ -5,6 +5,11 @@ def greet():
 def farewell():
     print("Goodbye, DevOps!")
 
+def add_number(a, b):
+    return a + b
+
 if __name__ == '__main__':
     greet()
     farewell()
+    result = add_number(5, 7)
+    print(f"The sum of 5 and 7 is {result}")
